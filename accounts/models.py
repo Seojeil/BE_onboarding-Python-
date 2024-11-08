58ed9f6294823612ad95b85b39215611dfa4b923
jeil <sjl01002@gmail.com>
2024-11-08 18:30:28 +0900

    add:User 모델 추가
    역할을 정의하기 위한 Role 모델도 함께 추가함
    admin, staff, user 중 선택이 가능하며 디폴트 값은  user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Role(models.Model):
+    ROLE_CHOICES = [
+        ("A", "ADMIN"),
+        ("S", "STAFF"),
+        ("U", "USER"),
+    ]
+
+    name = models.CharField(
+        max_length=50, choices=ROLE_CHOICES, default="U", unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=20, unique=True)
+    roles = models.ManyToManyField(Role, related_name="users", blank=True)
+
+    def __str__(self):
+        return self.username
