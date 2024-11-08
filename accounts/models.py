4cb64c1a2763c2ef53b40ee963680948fe9d8e62
jeil <sjl01002@gmail.com>
2024-11-08 18:39:47 +0900

    add:마이그레이션 생성

--- accounts/models.py
+++ accounts/models.py
@@ -21,5 +21,13 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=20, unique=True)
     roles = models.ManyToManyField(Role, related_name="users", blank=True)
 
+    # 기본 fields에 대한 related_name 지정
+    groups = models.ManyToManyField(
+        'auth.Group', related_name='custom_user_groups', blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission', related_name='custom_user_permissions', blank=True
+    )
+
     def __str__(self):
         return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
+
+
+User = get_user_model()
 
 
 class RegisterAPIView(APIView):
