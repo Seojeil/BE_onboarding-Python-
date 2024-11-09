d856a423ddff441a787709d0ffcb94a1428c6842
jeil <sjl01002@gmail.com>
2024-11-09 16:51:26 +0900

    feat:로그인 기능 추가
    로그인 성공시 refresh토큰을 발급함

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("register/", views.RegisterAPIView.as_view(), name="register"),
+    path("signin/", views.SignInAPIView.as_view(), name="signin"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 
@@ -17,3 +18,28 @@ class RegisterAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignInAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        # 사용자 인증
+        if user is None:
+            return Response(
+                {"detail": "사용자 정보가 일치하지 않습니다."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        # 토큰 발급
+        token = RefreshToken.for_user(user)
+
+        return Response(
+            {
+                "token": str(token),
+            },
+            status=status.HTTP_200_OK,
+        )
