a8883134abbdde439c2b096c7aa4529e7dee9259
jeil <sjl01002@gmail.com>
2024-11-09 13:55:08 +0900

    add:ceatesuperuser 명령 수정
    admin유저를 생성할 때, 이메일입력을 요구하지 않고 닉네입을 입력하도록
    수정함, 추가로 생성할 때, role을 A(admin)으로 하도록 지정함

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def create_superuser(self, nickname, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        extra_fields.setdefault('roles', 'A')
+
+        return self._create_user(nickname, None, password, **extra_fields)
 
 
 class Role(models.Model):
@@ -20,6 +35,8 @@ class Role(models.Model):
 class User(AbstractUser):
     nickname = models.CharField(max_length=20, unique=True)
     roles = models.ManyToManyField(Role, related_name="users", blank=True)
+
+    REQUIRED_FIELDS = ["nickname"]
 
     def __str__(self):
         return self.username
