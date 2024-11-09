995a66e267076b416cf1131a15f3483dd9c62e9f
jeil <sjl01002@gmail.com>
2024-11-09 15:59:59 +0900

    feat:회원가입 기능 추가
    유저네임, 비밀번호, 닉네임을 입력받으며 회원가입 후에 유저네임, 닉네임,
    역할을 출력하도록 함

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,4 +5,25 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ["username", "password", "nickname", "role"]
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
+
+    # 출력 데이터를 보여주도록 변환
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["role"] = instance.get_role_display()
+        return representation
