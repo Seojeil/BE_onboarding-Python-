c11d701d389e54b6639aed43a1ca165289575e35
jeil <sjl01002@gmail.com>
2024-11-09 14:26:50 +0900

    fix:createsuperuser 수정
    명령어를 사용했을 때, 권한이 제대로 설정되지 않는 오류를 수정함

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 class CustomUserManager(UserManager):
     def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         extra_fields.setdefault('role', 'A')
 
         return self._create_user(username, None, password, **extra_fields)
